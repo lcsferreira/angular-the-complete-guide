e4fb4557f33a6b4819684392354c20a9b7849ad1
Lucas Ferreira <lucas.simoes.ferreira@gmail.com>
2022-02-15 08:55:43 -0300

    local references

--- cmp-databinding-start/src/app/cockpit/cockpit.component.ts
+++ cmp-databinding-start/src/app/cockpit/cockpit.component.ts
@@ -9,15 +9,15 @@ export class CockpitComponent implements OnInit {
   @Output() serverCreated = new EventEmitter<{ serverName: string, serverContent: string }>();
   @Output("bpCreated") blueprintCreated = new EventEmitter<{ blueprintName: string, blueprintContent: string }>();
 
-  newServerName = '';
   newServerContent = '';
 
-  onAddServer() {
-    this.serverCreated.emit({ serverName: this.newServerName, serverContent: this.newServerContent });
+  onAddServer(nameInput: HTMLInputElement) {
+    console.log(nameInput.value)
+    this.serverCreated.emit({ serverName: nameInput.value, serverContent: this.newServerContent });
   }
 
-  onAddBlueprint() {
-    this.blueprintCreated.emit({ blueprintName: this.newServerName, blueprintContent: this.newServerContent });
+  onAddBlueprint(nameInput: HTMLInputElement) {
+    this.blueprintCreated.emit({ blueprintName: nameInput.value, blueprintContent: this.newServerContent });
   }
 
   constructor() { }
