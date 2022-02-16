4ab822a56da2be8e700ea035a9a2bcea33c4e9dd
Lucas Ferreira <lucas.simoes.ferreira@gmail.com>
2022-02-16 16:15:57 -0300

    Feature adding new ingredient to shopping list

--- recipe-book/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ recipe-book/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, EventEmitter, OnInit, Output, ViewChild } from '@angular/core';
+import { Ingredient } from 'src/app/shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-edit',
@@ -6,6 +7,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-edit.component.css']
 })
 export class ShoppingEditComponent implements OnInit {
+  @Output() shoppingItemAdded = new EventEmitter<Ingredient>();
+
+  @ViewChild('nameInput', { static: true }) nameInput: ElementRef;
+  @ViewChild('amountInput', { static: true }) amountInput: ElementRef;
+
+  onAddItem() {
+    const newIngredient = new Ingredient(this.nameInput.nativeElement.value, this.amountInput.nativeElement.value)
+    this.shoppingItemAdded.emit(newIngredient);
+  }
 
   constructor() { }
 
--- recipe-book/src/app/shopping-list/shopping-list.component.ts
+++ recipe-book/src/app/shopping-list/shopping-list.component.ts
@@ -12,6 +12,10 @@ export class ShoppingListComponent implements OnInit {
     new Ingredient("Tomatoes", 10),
   ];
 
+  onAddShoppingList(ingredient: Ingredient) {
+    this.ingredients.push(ingredient);
+  }
+
   constructor() { }
 
   ngOnInit(): void {
