439d594ceb3c581d40fabd32fa8255398b924bcf
Lucas Ferreira <lucas.simoes.ferreira@gmail.com>
2022-02-16 15:52:02 -0300

    passaing selected recipe to recipe detail

--- recipe-book/src/app/recipes/recipe-list/recipe-list.component.ts
+++ recipe-book/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -13,9 +13,15 @@ export class RecipeListComponent implements OnInit {
     new Recipe("A Test Recipe", "This is simply a test", "https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg")
   ];
 
+  @Output() recipeWasSelected = new EventEmitter<Recipe>()
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onRecipeSelected(recipeSelected: Recipe) {
+    this.recipeWasSelected.emit(recipeSelected)
   }
 
 }
